d0b6001c5d97ce47ff4975b72650fed1ece86bca
James Schumann <jschumann@gmail.com>
2016-08-22 14:55:01 -0400

    initial commit of plugin header

---
+++ wrdsb-kitchen-sink.php
@@ -0,0 +1,13 @@
+<?php
+/*
+* Plugin Name: WRDSB Kitchen Sink
+* Plugin URI: https://github.com/wrdsb/wordpress-plugin-wrdsb-kitchen-sink
+* Description: A bunch of semi-random features and functions too small for individual plugins
+* Author: WRDSB
+* Author URI: https://github.com/wrdsb
+* Version: 0.0.1
+* License: GPLv3 or later
+* GitHub Plugin URI: wrdsb/wordpress-plugin-wrdsb-kitchen-sink
+* GitHub Branch: master
+*/
+
