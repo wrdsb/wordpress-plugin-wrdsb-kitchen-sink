b02b8ac6c8410f37a53afa59286241eff60b4c89
James Schumann <jschumann@gmail.com>
2016-08-22 14:57:41 -0400

    disable password change notification emails

--- wrdsb-kitchen-sink.php
+++ wrdsb-kitchen-sink.php
@@ -11,3 +11,7 @@
 * GitHub Branch: master
 */
 
+# Disable password change notification emails
+if ( !function_exists( 'wp_password_change_notification' ) ) {
+    function wp_password_change_notification() {}
+}
